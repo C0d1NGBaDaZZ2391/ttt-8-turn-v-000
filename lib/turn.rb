a1d3cc4b43f1b543d7220eb6ba6d164af9ab4080
Bertha Muteba <nyc_bm26@aol.com>
2018-08-28 18:48:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,7 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if index.between?(0, 8)
+  if index.between?(1, 9)
     if position_taken?(board, index)
       return false
     end
